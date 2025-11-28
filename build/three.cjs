9274e865a71a654d071109435053f19e8dcbe704
Akira Kunishige <kira924age@gmail.com>
2025-11-28 15:11:56 +0900

    update WebGLMaterials.js

--- build/three.cjs
+++ build/three.cjs
@@ -27820,12 +27820,7 @@ function WebGLMaterials( renderer, properties ) {
 
 		if ( envMap ) {
 
-			// TODO HACK currently handling directly in WebGLRenderer only for MeshStandardMaterial for ReflectionProbes
-			if ( ! material.isMeshStandardMaterial ) {
-
-				uniforms.envMap.value = envMap;
-
-			}
+			uniforms.envMap.value = envMap;
 
 			_e1.copy( envMapRotation );
 
